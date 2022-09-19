99143d2213cd393dad353d762f6f05eec25d61a2
TomiA <tomi.ahola@gmail.com>
2022-09-19 11:08:03 +0300

    small change to test push to deploy

--- Controllers/CalcController.cs
+++ Controllers/CalcController.cs
@@ -2,16 +2,18 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace unittest.Controllers
 {
+    
     [ApiController]
     [Route("api/[controller]")]
     public class CalcController : ControllerBase
     {
+        
         [HttpGet("{a}/{b}")]
 
         public IActionResult CalculateSum(int a, int b)
         {
             int sum = a + b;
-            string res = "{'sum' : " + sum + "}";
+            string res = "{'sum1' : " + sum + "}";
             Console.WriteLine(res);
             return Ok(res);
         }
